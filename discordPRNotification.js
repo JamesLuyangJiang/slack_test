e3c0b3c81c0c9f423f43d6a6a50f809c2c2c39c3
James Jiang <luyang.jiang2002@outlook.com>
2023-11-18 05:16:14 -0800

    discord pr client setup

--- discordPRNotification.js
+++ discordPRNotification.js
@@ -1,9 +1,9 @@
-const { Client } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 
 const discordToken = process.env.DISCORD_TOKEN;
-const discordChannelId = 'YOUR_DISCORD_CHANNEL_ID';
+const discordChannelId = '1175239195749535786';
 
-const client = new Client();
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.login(discordToken);
 
 client.on('ready', () => {
