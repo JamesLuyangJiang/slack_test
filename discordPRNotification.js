435e37d1c3afa04683cde387bf27ee36980d22a2
James Jiang <luyang.jiang2002@outlook.com>
2023-11-18 05:25:13 -0800

    dc pr info fix

--- discordPRNotification.js
+++ discordPRNotification.js
@@ -1,20 +1,28 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const { Octokit } = require('@octokit/rest');
 
+const octokit = new Octokit();
 const discordToken = process.env.DISCORD_TOKEN;
 const discordChannelId = '1175239195749535786';
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.login(discordToken);
 
-client.on('ready', () => {
+client.on('ready', async () => {
   const channel = client.channels.cache.get(discordChannelId);
   if (channel) {
     const prNumber = process.env.GITHUB_EVENT_NUMBER;
-    const prUrl = process.env.GITHUB_SERVER_URL + '/' + process.env.GITHUB_REPOSITORY + '/pull/' + prNumber;
-    const prTitle = process.env.GITHUB_EVENT_PULL_REQUEST_TITLE;
-    const prUser = process.env.GITHUB_EVENT_PULL_REQUEST_USER_LOGIN;
-    const sourceBranch = process.env.GITHUB_EVENT_PULL_REQUEST_HEAD_REF;
-    const targetBranch = process.env.GITHUB_EVENT_PULL_REQUEST_BASE_REF;
+    const { data: pullRequest } = await octokit.pulls.get({
+      owner: process.env.GITHUB_REPOSITORY_OWNER,
+      repo: process.env.GITHUB_REPOSITORY,
+      pull_number: prNumber,
+    });
+
+    const prUrl = pullRequest.html_url;
+    const prTitle = pullRequest.title;
+    const prUser = pullRequest.user.login;
+    const sourceBranch = pullRequest.head.ref;
+    const targetBranch = pullRequest.base.ref;
 
     const message = `New pull request submitted: ${prUrl} by ${prUser}. Source branch: ${sourceBranch}, target branch: ${targetBranch}`;
     channel.send(message);
